053f74fa6612fe2da1312ae4facfd559866fb0e8
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-02-10 12:54:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clavisbct::Application.config.secret_token = '90f01338a7dfec7d00a2626088952251387567fe4223d68210716cfb07669d049907ed1e96f26af74845e97ca4fe11af1b3ac90cbdc87a5bd1fc138926f3575d'
