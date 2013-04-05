89c62df6d94f15cf87b81c414539c27627b27b3c
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-04-05 12:01:13 +0200

    'random' parameter added

--- app/controllers/sp_items_controller.rb
+++ app/controllers/sp_items_controller.rb
@@ -1,9 +1,18 @@
 class SpItemsController < ApplicationController
   def show
-    @sp_item=SpItem.find(params[:id])
+    if params[:id]=='random'
+      @sp_item=SpItem.find_by_sql('select * from sp.sp_items order by random() limit 1').first
+      @refresh = true
+    else
+      @sp_item=SpItem.find(params[:id])
+    end
     @h1_title='Proposte bibliografiche'
     @h1_link="/sp_bibliographies"
-    @h2_title=@sp_item.sp_section.sp_bibliography.title
+    if @sp_item.sp_section.nil?
+      @h2_title=@sp_item.sp_bibliography.title
+    else
+      @h2_title=@sp_item.sp_section.sp_bibliography.title
+    end
     @css_id='bibliografie'
     render :layout=>params[:layout] if !params[:layout].blank?
   end
