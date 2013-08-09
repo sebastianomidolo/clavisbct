41a525f2b9b649ac722c31c97ddbf293abbb4398
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-08-09 11:36:32 +0200

    Add has_many :dng_sessions

--- app/models/clavis_patron.rb
+++ app/models/clavis_patron.rb
@@ -5,6 +5,7 @@ class ClavisPatron < ActiveRecord::Base
   self.primary_key='patron_id'
 
   has_many :loans, :class_name=>'ClavisLoan', :foreign_key=>'patron_id'
+  has_many :dng_sessions, :foreign_key=>'patron_id'
 
   # Esempio: '70067cfc7e1429cfd7b710a19519d913027eb7a3','158.102.56.204, 158.102.162.9'
   def register_dng_login(opac_secret,client_ip)
