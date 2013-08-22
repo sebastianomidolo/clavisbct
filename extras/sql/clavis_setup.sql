b69c3bdb3275ab14b40d1229022713b54e055a8e
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-08-22 17:08:47 +0200

    Add some sql statements

--- extras/sql/clavis_setup.sql
+++ extras/sql/clavis_setup.sql
@@ -2,8 +2,8 @@ CREATE INDEX clavis_item_collocation_idx on clavis.item(collocation);
 update clavis.manifestation SET bid_source='SBN_bad_bid' where length(bid)!=10 and bid_source='SBN';
 
 
-create index clavis_item_manifestation_id_ndx on item(manifestation_id);
-create index clavis_item_title_ndx on item(to_tsvector('simple', title));
+create index clavis_item_manifestation_id_ndx on clavis.item(manifestation_id);
+create index clavis_item_title_ndx on clavis.item(to_tsvector('simple', title));
 
 UPDATE clavis.patron SET opac_username = lower(opac_username) WHERE opac_enable='1';
 CREATE INDEX clavis_patron_opac_username_idx on clavis.patron(opac_username) WHERE opac_enable='1';
