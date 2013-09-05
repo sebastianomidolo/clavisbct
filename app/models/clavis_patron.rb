d977e84697560897a2c47eab516a9d3e268ce569
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-09-05 15:33:41 +0200

    Add autorizzato_download_pdf

--- app/models/clavis_patron.rb
+++ app/models/clavis_patron.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # lastmod 21 febbraio 2013
 
 class ClavisPatron < ActiveRecord::Base
@@ -24,6 +25,37 @@ class ClavisPatron < ActiveRecord::Base
     sql="select true from clavis.shelf_item where shelf_id = 1929 and object_id=#{self.patron_id}"
     r=ClavisPatron.connection.execute(sql).num_tuples
     r == 0 ? false : true
+  end
+
+  def autorizzato_download_pdf(clavis_manifestation)
+    # Esempio su manifestation 571777 e uid 52697 cioè:
+    # reload!;ClavisPatron.find(52697).autorizzato_download_pdf(ClavisManifestation.find(571777))
+    # Esempio di pdf libero (in quanto composto da oggetti a loro volta liberi):
+    # reload!;ClavisPatron.find(8959).autorizzato_download_pdf(ClavisManifestation.find(219606))
+    aut=true
+    scadenza=Time.now + 1.hour
+    return false if clavis_manifestation.d_objects.size==0
+    dob=clavis_manifestation.d_objects.first
+    dirname=File.dirname(dob.filename)
+    clavis_manifestation.d_objects.each do |o|
+      next if o.access_right_id==0
+      mid=o.xmltag(:mid).to_i
+      if mid==0
+        puts o.filename
+      end
+      next if mid!=clavis_manifestation.id
+      uid=o.xmltag(:uid).to_i
+      # puts "uid: #{uid}"
+      # uid=8959; #debug only
+      aut=false if uid!=self.id
+      sc = o.xmltag(:sc)
+      next if sc.blank?
+      mesi = sc.split[0].to_i
+      t1 = o.f_mtime + mesi.months
+      scadenza = t1 if scadenza > t1
+    end
+    return true if aut==true
+    Time.now >= scadenza
   end
 
 end
