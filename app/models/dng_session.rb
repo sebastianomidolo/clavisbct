bfb58f97678105f003be57201a2368698c720d63
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-09-05 15:55:28 +0200

    Add 'download_pdf' service; Add 'request' parameter to DngSession.access_control_key

--- app/models/dng_session.rb
+++ app/models/dng_session.rb
@@ -8,20 +8,19 @@ class DngSession < ActiveRecord::Base
     Digest::SHA2.hexdigest([ip,self.login_time].join(','))
   end
 
-  def check_service(service_name,dng_session,params,request)
-    return false if dng_session.nil?
-
-    if service_name=='talking_book'
-      # return dng_session.inspect + params.inspect
-      if (dng_session.patron.opac_username != params[:dng_user] or
-          dng_session.client_ip != DngSession.format_client_ip(request))
-        # return "#{dng_session.client_ip} != #{DngSession.format_client_ip(request)}"
-        return false
-      else
-        return dng_session.patron.autorizzato_al_servizio_lp
-      end
+  def check_service(service_name,params,request,authorizable_object=nil)
+    if (self.patron.opac_username != params[:dng_user] or
+        self.client_ip != DngSession.format_client_ip(request))
+      return false
     end
-
+    case service_name
+    when 'talking_book'
+      return self.patron.autorizzato_al_servizio_lp
+    when 'download_pdf'
+      # In this case, authorizable_object is the instance of ClavisManifestion we are going to check
+      # for authorization
+      return self.patron.autorizzato_download_pdf(authorizable_object)
+    end
     false
   end
 
@@ -29,17 +28,19 @@ class DngSession < ActiveRecord::Base
     (Time.now-self.login_time).to_i > 3600 ? true : false
   end
 
-  def DngSession.find_from_params(params)
+  def DngSession.find_by_params_and_request(params,request)
     return nil if params[:dng_user].blank?
     user=DngSession.connection.quote(params[:dng_user].downcase)
+    ip=DngSession.connection.quote(DngSession.format_client_ip(request))
     sql=%Q{SELECT s.* FROM dng_sessions s JOIN clavis.patron p USING(patron_id)
-            WHERE p.opac_username=#{user} ORDER BY s.id desc}
+            WHERE p.opac_username=#{user} AND client_ip=#{ip} ORDER BY s.id desc LIMIT 1}
+    logger.debug("DngSession.find_by_params_and_request =>\n#{sql}")
     DngSession.find_by_sql(sql).first
   end
 
   def DngSession.authenticate_from_params(params,request)
     return false if params[:ac].blank? or params[:dng_user].blank?
-    dng=self.find_from_params(params)
+    dng=self.find_by_params_and_request(params,request)
     return false if dng.nil?
     ac=params[:ac]
     ip=DngSession.format_client_ip(request)
@@ -51,8 +52,8 @@ class DngSession < ActiveRecord::Base
     [request.remote_ip, request.headers['REMOTE_ADDR']].uniq.join(', ')
   end
 
-  def DngSession.access_control_key(params)
-    dng = DngSession.find_from_params(params)
+  def DngSession.access_control_key(params,request)
+    dng = DngSession.find_by_params_and_request(params,request)
     return nil if dng.nil? or dng.expired?
     dng.generate_ac
   end
