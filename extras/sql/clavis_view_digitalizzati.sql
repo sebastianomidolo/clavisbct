daa7afb3f3d82457c277b26f4d282ba2bfeab0e6
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-09-24 09:58:39 +0200

    change view clavis.digitalizzati

--- extras/sql/clavis_view_digitalizzati.sql
+++ extras/sql/clavis_view_digitalizzati.sql
@@ -3,7 +3,7 @@
 
 CREATE OR REPLACE VIEW clavis.digitalizzati AS
 
-select bib_level,bib_type,created_by,bid_source, bid, manifestation_id, title
+select bib_level,bib_type,created_by,modified_by,bid_source, bid, manifestation_id, title
   from clavis.manifestation cm join
   (select attachable_id as manifestation_id from attachments
   where attachable_type='ClavisManifestation'
