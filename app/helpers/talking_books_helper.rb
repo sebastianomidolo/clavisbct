33b728c71fb81058143c90780d75b313c73b673c
sebastianomidolo <sebastiano.mdl@gmail.com>
2013-11-14 13:10:19 +0100

    Add talking_book_extra_data; Add link to clavis_manifestation(s)

--- app/helpers/talking_books_helper.rb
+++ app/helpers/talking_books_helper.rb
@@ -17,13 +17,30 @@ module TalkingBooksHelper
                          content_tag(:td, record[k]))
     end
     res=content_tag(:table, res.join.html_safe)
-    if !record.clavis_item.nil?
-      lnk=clavis_item_path(record.clavis_item)
-      res << link_to('Vedi esemplare in Clavis',lnk)
-    else
-      res << "Non trovato su Clavis"
+    record.clavis_manifestations.each do |cm|
+      lnk=clavis_manifestation_path(cm)
+      res << link_to("Vedi manifestation #{cm.id}",lnk)
+      res << content_tag(:div, clavis_manifestation_opac_preview(cm))
     end
     res
   end
+
+  def talking_book_extra_data(record)
+    res=[]
+
+    fields=[
+            :intestatio,
+            :abstract
+           ]
+
+    fields.each do |k|
+      next if record[k].blank?
+      res << content_tag(:tr, content_tag(:td, k) +
+                         content_tag(:td, record[k]))
+    end
+    res=content_tag(:table, res.join.html_safe)
+    res
+  end
+
 
 end
