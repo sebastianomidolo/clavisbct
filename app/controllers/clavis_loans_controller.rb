10e9e621dba5848cf0701615550a24a45ee65f2d
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-01-09 14:24:24 +0100

    Set sql timezone

--- app/controllers/clavis_loans_controller.rb
+++ app/controllers/clavis_loans_controller.rb
@@ -2,6 +2,7 @@
 
 class ClavisLoansController < ApplicationController
   def receipts
+    ActiveRecord::Base.connection.execute("SET DateStyle TO ISO, DMY; SET timezone TO 'GMT-1';")
     ldb=params[:loan_date_begin]
     params[:loan_date_begin]=Time.now.to_date - 1.day if ldb.blank?
     params[:library_id]=2 if params[:library_id].blank?
