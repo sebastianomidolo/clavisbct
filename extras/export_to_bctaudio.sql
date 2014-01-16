fc4b3f4d2eaf846eae151405684c69ca64973ba9
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-01-16 15:24:45 +0100

    Change xml element names etc

--- extras/export_to_bctaudio.sql
+++ extras/export_to_bctaudio.sql
@@ -2,31 +2,35 @@
 \pset border 0
 \pset pager f
 
-
+SELECT '<?xml version="1.0" encoding="UTF-8"?>';
 SELECT xmlcomment('inizio esportazione: ' || now());
-
-select '<root>';
+select '<dataroot xmlns:od="urn:schemas-microsoft-com:officedata" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="TabFonoteca.xsd" generated="' || now() || '">';
 select xmlelement(
- name record,
-  xmlattributes(cm.manifestation_id as opac_id, a.idvolume as bm_id),
+ name "TabFonoteca",
+  xmlattributes(cm.manifestation_id as opac_id, a.idvolume as bm_id, a.tipologia),
   xmlconcat(
    xmlelement(name titolo,
        CASE WHEN cm.title is null THEN
-           a.titolo || ' - ' || a.autore || ' - ' || a.interpreti
+           -- a.titolo || ' - ' || a.autore || ' - ' || a.interpreti
+           a.titolo
        ELSE trim(cm.title)
        END
    ),
+   CASE WHEN a.autore is not null THEN
+     xmlelement(name autore, a.autore)
+   END,
+   CASE WHEN a.interpreti is not null THEN
+     xmlelement(name interpreti, a.interpreti)
+   END,
    xmlelement(name collocazione,
     CASE WHEN ci.collocation is null THEN replace(collocazione,' ','') ELSE ci.collocation END))
   )
  from bm_audiovisivi.t_volumi a left
 join av_manifestations av using(idvolume) left join clavis.item ci
-using(manifestation_id) left join clavis.manifestation cm
-using(manifestation_id)
+on(av.manifestation_id=ci.manifestation_id) left join clavis.manifestation cm
+on(ci.manifestation_id=cm.manifestation_id)
 where a.collocazione is not null
---   and a.idvolume=41
-;
-
-
-select '</root>';
+-- and a.idvolume=41
+-- limit 2
+;select '</dataroot>';
 SELECT xmlcomment('fine importazione: ' || now());
