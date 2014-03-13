a9e5b28e95d8342960e8573d451df275e476ac15
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-03-13 09:42:01 +0100

    -

--- app/helpers/excel_files_helper.rb
+++ app/helpers/excel_files_helper.rb
@@ -1,8 +1,12 @@
 module ExcelFilesHelper
   def excel_files_index(records)
     res=[]
+    res << content_tag(:tr, content_tag(:th, 'Nome file') +
+                         content_tag(:th, 'Numero di celle'))
+
     records.each do |r|
-      res << content_tag(:tr, content_tag(:td, link_to(File.basename(r.file_name), excel_file_path(r))))
+      res << content_tag(:tr, content_tag(:td, link_to(File.basename(r.file_name), excel_file_path(r))) +
+                         content_tag(:td, r.number_of_cells))
     end
     content_tag(:table, res.join.html_safe)
   end
