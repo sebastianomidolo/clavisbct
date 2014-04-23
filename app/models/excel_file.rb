b044d2b9d7bcf4a6551e9f09e4942892d102fe00
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-04-23 11:46:25 +0200

    Add config_filename, config_data, load_config, write_config

--- app/models/excel_file.rb
+++ app/models/excel_file.rb
@@ -3,8 +3,29 @@ class ExcelFile < ActiveRecord::Base
   def basename
     File.basename(self.file_name)
   end
+
+  def config_filename
+    self.file_name.sub(/.xls$/,'.yml')
+  end
+  def config_data
+    File.exists?(self.config_filename) ? File.read(self.config_filename) : nil
+  end
+  def load_config
+    d=self.config_data
+    return {} if d.blank?
+    YAML.load(d)
+  end
+  def write_config(config_data)
+    puts "scrivo in #{self.config_filename}"
+    File.open(self.config_filename, 'w') {|f| f.write(YAML.dump(config_data))}
+  end
+
+
   def ExcelFile.summary
-    sql=%Q{SELECT ef.id,ef.file_name,count(*) as number_of_cells from excel_cells ec join excel_sheets es on(es.id=ec.excel_sheet_id) join excel_files ef on(ef.id=es.excel_file_id) group by ef.id,ef.file_name order by ef.file_name}
+    # sql=%Q{SELECT ef.id,ef.file_name,count(*) as number_of_cells from excel_cells ec join excel_sheets es on(es.id=ec.excel_sheet_id) join excel_files ef on(ef.id=es.excel_file_id) group by ef.id,ef.file_name order by ef.file_name}
+    sql=%Q{SELECT * from excel_files ef order by ef.file_name}
+    # puts sql
     ExcelFile.find_by_sql(sql)
   end
+
 end
