59d72dedefa04836a9caff0e96eac37ee0d66294
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-04-23 11:44:01 +0200

    Add ExcelSheetsHelper

--- app/helpers/excel_sheets_helper.rb
+++ app/helpers/excel_sheets_helper.rb
@@ -7,4 +7,84 @@ module ExcelSheetsHelper
     end
     content_tag(:table, res.join.html_safe)
   end
+
+  def excel_sheet_views(record)
+    res=[]
+    cnt=0
+    record.views.each do |v|
+      cnt+=1
+      res << content_tag(:li, link_to(v[:name],view_excel_sheet_path(record,view: cnt)))
+    end
+    return 'x' if cnt==0
+    content_tag(:ul, res.join.html_safe)
+  end
+
+  def excel_sheet_headings(sheet,view_number=nil)
+    res=[]
+    tdd=[]
+    cnt=0
+    sheet.sql_columns(view_number).each do |c|
+      if c=="excel_cell_row"
+        tdd << content_tag(:td, '')
+      else
+        colno=view_number.nil? ? "(#{cnt})" : ''
+        tdd << content_tag(:td, link_to(%Q{#{c.gsub('"','')}#{colno}},excel_sheet_path(sheet,{group:cnt,view_number:view_number})))
+
+      end
+      cnt+=1
+    end
+    content_tag(:tr, tdd.join.html_safe)
+  end
+
+  def excel_sheet_records_group_by(sheet,records,column_number,view_number=nil)
+    res=[]
+    content_tag(:table, res.join.html_safe, {class: 'table table-striped'})
+    records.each do |r|
+      lnk=link_to(r['col'],excel_sheet_path(sheet,{qs:r['col'],
+                                              column_number:column_number,
+                                              view_number:view_number}))
+      res << content_tag(:tr, content_tag(:td, lnk) +
+                         content_tag(:td, r['count']))
+    end
+    content_tag(:table, res.join.html_safe, {class: 'table table-striped'})
+  end
+
+  def excel_sheet_records(sheet,records,view_number=nil)
+    res=[]
+    res << content_tag(:tr, excel_sheet_headings(sheet,view_number))
+    records.each do |r|
+      tdd=[]
+      sheet.sql_columns(view_number).each do |c|
+        if c=='"manifestation_id"'
+          mid=r[c.gsub('"','')].to_i
+          if mid!=0
+            content="Vedi notizia #{mid} in Clavis: #{ClavisManifestation.find(mid).title}"
+            lnk=ClavisManifestation.clavis_url(mid,:show)
+            tdd << content_tag(:td, link_to(content,lnk))
+          else
+            tdd << content_tag(:td, "manca manifestation_id")
+          end
+        else
+          tdd << content_tag(:td, r[c.gsub('"','')])
+        end
+      end
+      res << content_tag(:tr, tdd.join.html_safe)
+    end
+    content_tag(:table, res.join.html_safe, {class: 'table table-striped'})
+  end
+
+  def excel_sheet_breadcrumbs(excel_sheet,view_number=nil)
+    res=[]
+    res << content_tag(:li, link_to('Index', excel_files_path))
+    res << content_tag(:li, link_to(File.basename(excel_sheet.excel_file.file_name),
+                                    excel_file_path(excel_sheet.excel_file)))
+    if !view_number.nil?
+      res << content_tag(:li, link_to(excel_sheet.sheet_name,excel_sheet_path(excel_sheet)))
+      res << content_tag(:li, excel_sheet.views[view_number-1][:name], class: 'active')
+    else
+      res << content_tag(:li, excel_sheet.sheet_name,class:'active')
+    end
+    content_tag(:ol, res.join.html_safe, class: 'breadcrumb')
+  end
+
 end
