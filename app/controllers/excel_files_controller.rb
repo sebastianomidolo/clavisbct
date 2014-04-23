333c5d7c33d6fe45dc81ebdf5b7358c2cdd340e7
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-04-23 11:29:47 +0200

    Add auto-sync in ExcelFilesController#show

--- app/controllers/excel_files_controller.rb
+++ app/controllers/excel_files_controller.rb
@@ -7,11 +7,7 @@ class ExcelFilesController < ApplicationController
 
   def show
     @excel_file=ExcelFile.find(params[:id])
-    if params[:qs].blank?
-      @excel_cells=[]
-    else
-      @excel_sheets=@excel_file.excel_sheets
-      @excel_cells=ExcelCell.where(:excel_sheet_id=>@excel_sheets).where("cell_content ~* '#{params[:qs]}'")
-    end
+    @excel_sheets=@excel_file.excel_sheets
+    @excel_sheets.each {|es| es.sync}
   end
 end
