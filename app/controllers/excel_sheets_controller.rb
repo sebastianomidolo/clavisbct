db834ecfb54392ff3c8c2cfc6a0238957dd36d6a
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-05-09 13:48:56 +0200

    Edit show

--- app/controllers/excel_sheets_controller.rb
+++ app/controllers/excel_sheets_controller.rb
@@ -2,8 +2,14 @@ class ExcelSheetsController < ApplicationController
   layout 'navbar'
 
   def show
-    @excel_sheet=ExcelSheet.find(params[:id])
+    if params[:id].to_i==0
+      @excel_sheet=ExcelSheet.find_by_tablename(params[:id])
+      render text: 'errore, tabella non trovata' and return if @excel_sheet.nil?
+    else
+      @excel_sheet=ExcelSheet.find(params[:id])
+    end
     @view_number=params[:view_number].blank? ? nil : params[:view_number].to_i
+    render template: '/excel_sheets/row' and return if !params[:row].blank?
     if params[:group].blank?
       @records=@excel_sheet.sql_paginate(params, :page=>params[:page])
     else
