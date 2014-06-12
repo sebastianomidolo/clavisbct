bfc873f95b7361f517120d8ae607b929b13f7304
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-06-12 08:45:26 +0200

    Edit clavis_libraries_celdes_libraries

--- app/helpers/clavis_libraries_helper.rb
+++ app/helpers/clavis_libraries_helper.rb
@@ -2,7 +2,7 @@ module ClavisLibrariesHelper
   def clavis_libraries_celdes_libraries
     sql=%Q{SELECT cl.label,library_id FROM public.biblioteche_celdes bc JOIN clavis.library cl using(library_id) ORDER BY cl.label;}
     res = []
-    year=Time.now.year
+    year=params[:year].blank? ? Time.now.year-1 : params[:year]
     ClavisLibrary.connection.execute(sql).each do |r|
       res << content_tag(:tr, content_tag(:td, link_to(r['label'], "periodici_ordini?library_id=#{r['library_id']}&year=#{year}")))
     end
