694a08e656bfa9f6a4e0491742ea280ba020f765
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-10-16 11:25:12 +0200

    Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
-
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :trackable, :validatable
-
-  # devise :database_authenticatable, :encryptable, :registerable
-  devise :database_authenticatable, :registerable
-
-  # Setup accessible (or protected) attributes for your model
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+
+  def google_doc_key
+    h={
+      civ:    '1dKUg08NvSWQmOy0oJ6lKsagHFIDi4bbkeqKbJgsBf9k',
+      copat1: '1aSJzKCI1_WlimHWtbgd5LircGAkEOrpCK90sH0n9ySs',
+      copat2: '1150XuZU2DcxiYc7URUKMYpgoVwmRMcpahSr1dAAmvMY',
+      copat3: '1XuRHleKDxgo_LtO4kGxaWEpxuNMxwIqN_7-gPuM9v9o',
+    }
+    h[self.email.to_sym]
+  end
 end
