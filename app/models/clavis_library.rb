05037b2fe639ffeda4610fd99a585856b1656faa
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-11-10 17:29:01 +0100

    -

--- app/models/clavis_library.rb
+++ app/models/clavis_library.rb
@@ -4,6 +4,7 @@ class ClavisLibrary < ActiveRecord::Base
 
   has_many :owned_items, class_name: 'ClavisItem', foreign_key: 'owner_library_id'
   has_many :ordini, foreign_key: 'library_id'
+  has_many :containers, foreign_key: 'library_id'
 
   def to_label
     self.label[0..40]
@@ -11,5 +12,17 @@ class ClavisLibrary < ActiveRecord::Base
   def nice_description
     self.description[5..100]
   end
+
+  def clavis_url
+    ClavisLibrary.clavis_url(self.id)
+  end
+
+  def ClavisLibrary.clavis_url(id)
+    config = Rails.configuration.database_configuration
+    host=config[Rails.env]['clavis_host']
+    "#{host}/index.php?page=Library.LibraryViewPage&id=#{id}"
+  end
+
+
   
 end
