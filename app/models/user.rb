4b2264b33bcc3bdd0361050bb1f0181b2806628f
sebastianomidolo <sebastiano.mdl@gmail.com>
2014-11-10 16:57:18 +0100

    User.googledrive_session

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
-  def google_doc_key
-    h={
-      civ:    '1dKUg08NvSWQmOy0oJ6lKsagHFIDi4bbkeqKbJgsBf9k',
-      copat1: '1aSJzKCI1_WlimHWtbgd5LircGAkEOrpCK90sH0n9ySs',
-      copat2: '1150XuZU2DcxiYc7URUKMYpgoVwmRMcpahSr1dAAmvMY',
-      copat3: '1XuRHleKDxgo_LtO4kGxaWEpxuNMxwIqN_7-gPuM9v9o',
-    }
-    h[self.email.to_sym]
+  def User.googledrive_session
+    config = Rails.configuration.database_configuration
+    username=config[Rails.env]["google_drive_login"]
+    passwd=config[Rails.env]["google_drive_passwd"]
+    GoogleDrive.login(username, passwd)
   end
 end
