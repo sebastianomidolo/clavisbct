8194c14e11cb351f9c79756b119e2fb5b05534b9
sebastianomidolo <sebastiano.mdl@gmail.com>
2015-04-01 13:49:56 +0200

    Ordini periodici: modifica importazione dati

--- extras/sql/clavis_create_serials_admin_table.sql
+++ extras/sql/clavis_create_serials_admin_table.sql
@@ -5,42 +5,62 @@ COMMIT;
 BEGIN;
 ALTER TABLE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
    ALTER COLUMN "data pagamento" type text using ("data pagamento"::text);
-ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
-   ALTER COLUMN "data pagamento" type text using ("data pagamento"::text);
 COMMIT;
 
 BEGIN;
 UPDATE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione set "data pagamento" = NULL
     WHERE "data pagamento"='';
 COMMIT;
+
 BEGIN;
-UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione set "data pagamento" = NULL
+ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione_2014
+   ALTER COLUMN "data pagamento" type text using ("data pagamento"::text);
+UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione_2014 set "data pagamento" = NULL
     WHERE "data pagamento"='';
+   ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione_2014   
+   ALTER COLUMN "data pagamento" type date using ("data pagamento"::date);
 COMMIT;
 
+BEGIN;
 set datestyle TO "DMY";
 ALTER TABLE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
    ALTER COLUMN "data pagamento" type date using ("data pagamento"::date);
-
-set datestyle TO "DMY";
-ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
-   ALTER COLUMN "data pagamento" type date using ("data pagamento"::date);
-
-BEGIN;
-UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione SET numero = NULL WHERE numero='';
 COMMIT;
+
+-- BEGIN;
+-- set datestyle TO "DMY";
+-- ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
+--    ALTER COLUMN "data pagamento" type date using ("data pagamento"::date);
+-- COMMIT;
+-- BEGIN;
+-- UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione SET numero = NULL WHERE numero='';
+-- COMMIT;
 
 
 BEGIN;
 ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
   ALTER COLUMN "numero" type integer using ("numero"::integer);
-
-UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione SET "totale articolo" = NULL
-  WHERE "totale articolo"='';
-ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
-  ALTER COLUMN "totale articolo" type float using ("totale articolo"::float);
-
 COMMIT;
+
+/*
+BEGIN;
+UPDATE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione SET "prezzo" = NULL
+  WHERE "prezzo"='';
+COMMIT;
+
+BEGIN;
+ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
+  ALTER COLUMN "prezzo" type float using ("prezzo"::float);
+COMMIT;
+*/
+
+/* patch temporanea in attesa che venga aggiunta la colonna "data pagamento" */
+
+BEGIN;
+ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
+  ADD COLUMN "data pagamento" date;
+COMMIT;
+
 
 set datestyle TO "DMY";
 CREATE TABLE public.serials_admin_table
@@ -73,7 +93,7 @@ select g.titolo, cc.manifestation_id,b.library_id,f.numero as numero_fattura,
 UNION
 -- musicale
 select o.titolo, cc.manifestation_id, 3 as library_id, f.numero as numero_fattura,
- f."totale articolo" as importo_fattura,"CIG",
+ f."prezzo" as importo_fattura,"CIG",
  "fattura / nota credito"::char(1) as fattura_o_nota_di_credito,
  "data fattura / nota credito"::date as data_emissione,
  "data pagamento"::date as data_pagamento,
@@ -85,9 +105,28 @@ select o.titolo, cc.manifestation_id, 3 as library_id, f.numero as numero_fattur
   from excel_files_tables.celdes_musicale_admin_report_ordini o
     left join excel_files_tables.ordini_periodici_musicale cc using(titolo)
     left join excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione
+     f using(titolo)
+UNION
+-- musicale 2014
+select o.titolo, cc.manifestation_id, 3 as library_id, f.numero as numero_fattura,
+ f."totale articolo" as importo_fattura,"CIG",
+ "fattura / nota credito"::char(1) as fattura_o_nota_di_credito,
+ "data fattura / nota credito"::date as data_emissione,
+ "data pagamento"::date as data_pagamento,
+ o.prezzo, o.commissione_sconto, o.totale, o.iva, o."prezzo finale" as prezzo_finale,
+ o.numcopie, o.ordnum, o.ordanno, o.ordprogressivo,
+ o.abbmesedal || '/' || o.abbannodal || '-' || o.abbmeseal || '/' || o.abbannoal as periodo,
+ o.statodettaglio,
+ o.tipo as formato, cc.note as note_interne
+  from excel_files_tables.celdes_musicale_admin_report_ordini_2014 o
+    left join excel_files_tables.ordini_periodici_musicale cc using(titolo)
+    left join excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione_2014
      f using(titolo);
 
 update public.serials_admin_table set fattura_o_nota_di_credito = upper(fattura_o_nota_di_credito);
 
 alter table serials_admin_table add column id serial primary key;
 update serials_admin_table set prezzo=null where prezzo='';
+
+
+alter table serials_admin_table alter COLUMN ordanno set default extract(year from now());
