e5b15650889088d0d2237069137e9b52f8acb824
sebastianomidolo <sebastiano.mdl@gmail.com>
2015-06-09 15:15:50 +0200

    -

--- extras/sql/clavis_create_serials_admin_table.sql
+++ extras/sql/clavis_create_serials_admin_table.sql
@@ -2,15 +2,30 @@ BEGIN;
 DROP TABLE public.serials_admin_table;
 COMMIT;
 
+
+/* patch temporanea in attesa che venga aggiunta la colonna "data pagamento" */
+BEGIN;
+ALTER TABLE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
+  ADD COLUMN "data pagamento" date;
+COMMIT;
 BEGIN;
 ALTER TABLE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
    ALTER COLUMN "data pagamento" type text using ("data pagamento"::text);
 COMMIT;
-
 BEGIN;
 UPDATE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione set "data pagamento" = NULL
     WHERE "data pagamento"='';
 COMMIT;
+
+BEGIN;
+ALTER TABLE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione_2014
+   ALTER COLUMN "data pagamento" type text using ("data pagamento"::text);
+COMMIT;
+BEGIN;
+UPDATE excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione_2014 set "data pagamento" = NULL
+    WHERE "data pagamento"='';
+COMMIT;
+
 
 BEGIN;
 ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatturazione_2014
@@ -62,10 +77,38 @@ ALTER TABLE excel_files_tables.celdes_musicale_admin_report_ordini_con_dati_fatt
   ADD COLUMN "data pagamento" date;
 COMMIT;
 
+update excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione set prezzo=NULL where prezzo='';
 
 set datestyle TO "DMY";
 CREATE TABLE public.serials_admin_table
 AS
+select g.titolo, cc.manifestation_id,b.library_id,f.numero as numero_fattura,
+ f."prezzo"::double precision as importo_fattura,"CIG",
+ "fattura / nota credito"::char(1) as fattura_o_nota_di_credito,
+ "data fattura / nota credito"::date as data_emissione,
+ "data pagamento"::date as data_pagamento,
+ o.prezzo, o.commissione_sconto, o.totale, o.iva, o."prezzo finale" as prezzo_finale,
+ o.numcopie, o.ordnum, o.ordanno, o.ordprogressivo,
+ o.abbmesedal || '/' || o.abbannodal || '-' || o.abbmeseal || '/' || o.abbannoal as periodo,
+ o.statodettaglio as stato,
+ g.formato,NULL as note_interne
+  from excel_files_tables.celdes_gest_riepilogo_situazione_ordini g
+   left join excel_files_tables.titoli_celdes_clavis cc using(titolo)
+   join excel_files_tables.celdes_admin_report_ordini o using(titolo,ordnum,ordprogressivo)
+   left join excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
+    f on(g.titolo=f.titolo and g.destinatario=f.destinatario
+----> Nota che la join andrebbe fatta usando o.ordanno,o.ordnum e o.ordprogressivo
+----> che però a oggi (giugno 2014) mancano nella tabella f
+     and g.abbmesedal=f.abbmesedal
+     and g.abbmeseal=f.abbmeseal
+     and g.abbannodal=f.abbannodal
+     and g.abbannoal=f.abbannoal
+     and g.abbvoldal=f.abbvoldal
+     and g.abbvolal=f.abbvolal
+    )
+   join biblioteche_celdes b on(b.label=g.destinatario)
+UNION
+-- BCT 2014
 select g.titolo, cc.manifestation_id,b.library_id,f.numero as numero_fattura,
  f."totale articolo" as importo_fattura,"CIG",
  "fattura / nota credito"::char(1) as fattura_o_nota_di_credito,
@@ -79,7 +122,7 @@ select g.titolo, cc.manifestation_id,b.library_id,f.numero as numero_fattura,
   from excel_files_tables.celdes_gest_riepilogo_situazione_ordini g
    left join excel_files_tables.titoli_celdes_clavis cc using(titolo)
    join excel_files_tables.celdes_admin_report_ordini o using(titolo,ordnum,ordprogressivo)
-   left join excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione
+   left join excel_files_tables.celdes_admin_report_ordini_con_dati_fatturazione_2014
     f on(g.titolo=f.titolo and g.destinatario=f.destinatario
 ----> Nota che la join andrebbe fatta usando o.ordanno,o.ordnum e o.ordprogressivo
 ----> che però a oggi (giugno 2014) mancano nella tabella f
