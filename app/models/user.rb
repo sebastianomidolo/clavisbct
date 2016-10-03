e8d3abc85d7bbac8e681c286fc78c788fe4cebdc
sebastianomidolo <sebastiano.mdl@gmail.com>
2016-10-03 17:16:53 +0200

    Modificato User: eliminato :registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :registerable
 
-  devise :database_authenticatable,:registerable, :recoverable, :rememberable, :trackable, :validatable
-  # devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable,:registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :encrypted_password, :created_at, :updated_at
 
