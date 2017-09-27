614fad379b66ab9facb1b1d1963c46aa9eb3a21f
sebastianomidolo <sebastiano.mdl@gmail.com>
2017-09-27 18:36:40 +0200

    home@senza_piano

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class HomeController < ApplicationController
   layout 'navbar'
   # before_filter :authenticate_user!, only: 'bidcr'
@@ -34,11 +35,38 @@ class HomeController < ApplicationController
     @sommario=ActiveRecord::Base.connection.execute(sql).to_a
   end
 
-
+  def senzapiano
+    sql="select item_id from clavis.centrale_locations where piano is null limit 10"
+    ids=ActiveRecord::Base.connection.execute(sql).to_a.collect {|i| i['item_id']}
+    redirect_to clavis_items_path(item_ids:ids.join('+'))
+  end
+  
   def spazioragazzi
     render :text=>File.read('/tmp/indexfile.html')
   end
 
+  def senzasoggetto
+    sql=%Q{
+with manifestations as (
+select distinct cm.manifestation_id,cm.bib_level,cm.bib_type,cm.created_by,ci.item_id,
+  ci.inventory_serie_id
+ from clavis.manifestation cm
+ join clavis.item ci using(manifestation_id)
+left join
+ clavis.l_authority_manifestation am using(manifestation_id) left join
+ clavis.authority ca using(authority_id) where am.link_type isnull
+ and ci.home_library_id=2
+)
+select cm.* from manifestations cm left join
+  clavis.l_manifestation lm on(cm.manifestation_id in(manifestation_id_up,manifestation_id_down))
+  where cm.bib_level='m' and cm.bib_type='a01' and lm is null
+  and cm.inventory_serie_id not in ('CLA','TAT') and cm.created_by=1;
+}
+    ids=ActiveRecord::Base.connection.execute(sql).to_a.collect {|i| i['item_id']}
+    user_session[:item_ids]=nil
+    redirect_to clavis_items_path(item_ids:ids.join(' '),sql:sql,per_page:9999)
+  end
+  
   def uni856
     @pagetitle='Titoli in Clavis con URL (unimarc tag 856)'
     sql=%Q{select trim(cm.title) as title,u.* from clavis.uni856 u
