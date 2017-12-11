df73a87e6816d40f3cf3edcb1fb735efd5e5fe73
sebastianomidolo <sebastiano.mdl@gmail.com>
2017-12-11 17:41:29 +0100

    add store_user_location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_filter :store_user_location!, if: :storable_location?
   protect_from_forgery
   layout 'navbar'
   rescue_from CanCan::AccessDenied do |exception|
     render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: true
   end
+
+  private
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
 end
