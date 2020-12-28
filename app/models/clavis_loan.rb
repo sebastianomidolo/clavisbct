2b844fdc9f3bd543bba18cfe3a8495a4cfb1f52c
sebastianomidolo <sebastiano.mdl@gmail.com>
2020-12-28 18:49:33 +0100

    Add ClavisLoan.loans_report

--- app/models/clavis_loan.rb
+++ app/models/clavis_loan.rb
@@ -73,6 +73,68 @@ class ClavisLoan < ActiveRecord::Base
     return ClavisLoan.find_by_sql(sql)
   end
 
+  def ClavisLoan.loans_report(params={})
+    cond=[]
+    cond << "item_id!=0"
+    # cond << "loan_date_begin >= #{self.connection.quote(params[:loan_date_begin])}" if !params[:loan_date_begin].blank?
+    # cond << "loan_date_end   <= #{self.connection.quote(params[:loan_date_end])}"   if !params[:loan_date_end].blank?
+    # cond << "loan_date_begin BETWEEN '#{params[:begin_from]}' AND '#{params[:begin_to]}'" if !params[:begin_from].blank? AND !params[:begin_to].blank?
+    cond << "loan_date_begin BETWEEN '#{params[:begin_from]}' AND '#{params[:begin_to]}'"
+    where = cond == [] ? '' : "WHERE #{cond.join(' and ')}"
+    sql_loans=%Q{SELECT md5(patron_id::text) as patron_id_md5,
+                  patron_id,
+                  item_id,
+                  loan_date_begin,
+                  loan_date_end,
+                  due_date,
+                  from_library,
+                  to_library,
+                  end_library
+                  FROM clavis.loan #{where}}
+    puts sql_loans
+    tn="reports.loans"
+    self.connection.execute("DROP TABLE if exists #{tn}; CREATE TABLE #{tn} as #{sql_loans}")
+
+    sql_patrons = %Q{
+       SELECT patron_id, md5(patron_id::text) as patron_id_md5,
+         registration_library_id, preferred_library_id, gender, title, civil_status, date_created, last_seen
+            FROM clavis.patron where patron_id in (select patron_id from reports.loans)}
+    tn="reports.patrons"
+    self.connection.execute("DROP TABLE if exists #{tn}; CREATE TABLE #{tn} as #{sql_patrons}")
+
+    sql_items = %Q{
+       SELECT item_id, manifestation_id, item_media, home_library_id,
+              inventory_date
+       FROM clavis.item where item_id in (select item_id from reports.loans);
+    }
+    tn="reports.items"
+    self.connection.execute("DROP TABLE if exists #{tn}; CREATE TABLE #{tn} as #{sql_items}")
+
+    sql_manifestations = %Q{
+       SELECT manifestation_id,
+               -- bib_level, bib_type, bib_type_first, manifestation_status,
+               edition_language, edition_date, "ISBNISSN", "EAN",
+               trim(title) as title, author, publisher, loanable_since, date_created
+       FROM clavis.manifestation where manifestation_id in (select manifestation_id from reports.items);
+    }
+    tn="reports.manifestations"
+    self.connection.execute("DROP TABLE if exists #{tn}; CREATE TABLE #{tn} as #{sql_manifestations}")
+
+    self.connection.execute(%Q{
+           alter table reports.loans drop column patron_id;
+           alter table reports.patrons drop column patron_id;
+           create unique index reports_patrons_patron_id_md5 on reports.patrons(patron_id_md5);
+           alter table reports.loans add constraint patron_id_md5_fkey FOREIGN KEY(patron_id_md5)
+             REFERENCES reports.patrons(patron_id_md5);
+           create unique index reports_items_item_id on reports.items(item_id);
+           alter table reports.loans add constraint item_id_fkey FOREIGN KEY(item_id) REFERENCES reports.items(item_id);
+           -- Questi sono i fuori catalogo, che ci danno un bel problema:
+           update reports.items set manifestation_id=NULL where manifestation_id=0;
+           create unique index reports_manifestations_manifestation_id on reports.manifestations(manifestation_id);
+           alter table reports.items add constraint manifestation_id_fkey FOREIGN KEY(manifestation_id)
+               REFERENCES reports.manifestations(manifestation_id)})
+  end
+
   def ClavisLoan.loans_by_supplier(supplier_id, params)
     page = params[:page].blank? ? 1 : params[:page].to_i
     per_page = params[:per_page].blank? ? 40 : params[:per_page].to_i
