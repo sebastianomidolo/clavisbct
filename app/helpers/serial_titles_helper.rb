d67c3e64363a0becf3148e95068f9403a126e4d2
sebastianomidolo <sebastiano.mdl@gmail.com>
2022-01-25 10:01:48 +0100

    allineamento helper serial_titles

--- app/helpers/serial_titles_helper.rb
+++ app/helpers/serial_titles_helper.rb
@@ -4,39 +4,32 @@ module SerialTitlesHelper
   def serial_titles_list_readonly(records)
     res=[]; cnt=0
     records.each do |r|
-      library_names = params[:includi_bib].blank? ? '' : r.library_names
-      num_tit = params[:numera_titoli].blank? ? '' : cnt+=1
-      prezzo_stimato=prezzo_totale_stimato=''
-      if !params[:includi_prezzi].blank?
-        prezzo_stimato = r.prezzo_stimato
-        prezzo_totale_stimato = r.prezzo_totale_stimato
-      end
+      note = r.note.blank? ? '' : "<br/>Nota interna: <em>#{r.note}</em>"
+      freq = r.frequency_label.blank? ? '' : " [<em>#{r.frequency_label}</em>]"
 
-      num_copie = params[:includi_numcopie].blank? ? '' : r.tot_copie
-      note = r.note.blank? ? '' : "<br/><em>#{r.note}</em>"
-      title="#{r.title}#{note}".html_safe
-      if params[:library_id].to_i>0 and params[:includi_bib].blank?
-        n=SerialSubscription.copie_per_bib(params[:library_id].to_i, r.libraries, r.numero_copie)
-        title << " (#{n} copie)" if n>1
-      end
+      mytit = "#{r.title}#{freq}"
+      title = r.manifestation_id.nil? ? r.title : link_to(mytit.html_safe, ClavisManifestation.clavis_url(r.manifestation_id), {class:'',target:'_new'})
 
-      libraries=SerialSubscription.associa_copie_multiple(r.library_names,r.numero_copie) if !params[:includi_bib].blank?
-      res << content_tag(:tr, content_tag(:td, num_tit) +
+      libraries = params[:library_id].blank? ? SerialSubscription.associa_copie_multiple(r.library_names,r.numero_copie) : ''
+      res << content_tag(:tr, content_tag(:td, cnt+=1) +
                               content_tag(:td, title) +
-                              content_tag(:td, prezzo_stimato) +
-                              content_tag(:td, prezzo_totale_stimato) +
-                              content_tag(:td, libraries) +
-                              content_tag(:td, num_copie))
+                              content_tag(:td, libraries))
     end
-    content_tag(:table, res.join.html_safe, {class: 'table table-bordered table-condensed table-striped'})
+    content_tag(:table, res.join("\n").html_safe, {class: 'table table-bordered table-condensed table-striped'})
   end
 
   def serial_titles_list(records)
     res=[]; cnt=0
     records.each do |r|
       lnk = r.tot_copie=='0' ? link_to('Elimina', r, method: :delete, data: { confirm: 'Confermi cancellazione titolo?' }) : ''
-      note = r.note.blank? ? '' : "<br/><em>#{r.note}</em>"
-      title=link_current_params("#{r.title}#{note}".html_safe, edit_serial_title_path(r), params)
+      note = r.note.blank? ? '' : "<br/>Nota interna: <em>#{r.note}</em>"
+      note_fornitore = r.note_fornitore.blank? ? '' : "<br/>Nota fornitore: <em>#{r.note_fornitore}</em>"
+      freq = r.frequency_label.blank? ? '' : " [<em>#{r.frequency_label}</em>]"
+      publisher = r.publisher.blank? ? '' : ". - #{r.publisher}"
+      # title=link_current_params("#{r.title}#{freq}#{publisher}#{note}#{note_fornitore}".html_safe, edit_serial_title_path(r), params)
+      clavis_lnk= r.manifestation_id.nil? ? '' : link_to("Clavis", ClavisManifestation.clavis_url(r.manifestation_id), {class:'btn btn-success',target:'_new'})
+      title=link_current_params("#{r.title}#{freq}#{publisher}".html_safe, edit_serial_title_path(r), params) +
+            clavis_lnk + content_tag(:span, "#{note}#{note_fornitore}".html_safe)
 
       if r.tot_copie.to_i > 1
         totale = "#{r.prezzo_totale_stimato} (#{r.tot_copie}&nbsp;copie)".html_safe
@@ -44,16 +37,47 @@ module SerialTitlesHelper
         totale = ''
       end
       libraries=SerialSubscription.associa_copie_multiple(r.library_names,r.numero_copie)
+      lnklib=link_current_params(libraries, serial_title_path(r), params)
+      if !params[:library_id].blank? and !r.manifestation_id.nil? and params[:items_details]=='t'
+        title << serial_titles_issues_report(r)
+      end
       res << content_tag(:tr, content_tag(:td, cnt+=1) +
                               content_tag(:td, title) +
                               content_tag(:td, r.prezzo_stimato) +
                               content_tag(:td, totale) +
-                              content_tag(:td, link_current_params(libraries, serial_title_path(r), params)) +
+                              content_tag(:td, lnklib) +
                               content_tag(:td, lnk))
     end
     content_tag(:table, res.join.html_safe, {class: 'table table-bordered table-condensed table-striped'})
   end
 
+  def serial_titles_invoice_select(records, serial_invoice, library_id)
+    res=[]; cnt=0
+    prst=pr=0
+    records.each do |r|
+      freq = r.frequency_label.blank? ? '' : " [<em>#{r.frequency_label}</em>]"
+      mytit = "#{r.title}#{freq}"
+
+      price = r.invoice_ids.blank? ? '' : invoice_editable(r, serial_invoice.id, library_id)
+      prst += r.prezzo_stimato if !r.prezzo_stimato.nil?
+      # pr += r.prezzo_in_fattura
+      checkbox=check_box_tag("titles_invoice[#{r.id}]", 't', (r.invoice_ids.blank? ? false : true), :onchange=>'')
+      label = label_tag("titles_invoice[#{r.id}]", r.title)
+      res << content_tag(:tr, content_tag(:td, checkbox) +
+                              content_tag(:td, label) +
+                              content_tag(:td,r.prezzo_stimato) +
+                              content_tag(:td, price))
+      res << hidden_field_tag("titles[#{r.id}]", true)
+    end
+    res << content_tag(:tr, content_tag(:td, '') +
+                            content_tag(:td, '') +
+                            content_tag(:td,prst) +
+                            content_tag(:td,'-'))
+    content_tag(:table, res.join("\n").html_safe, {class: 'table table-bordered table-condensed table-striped'})
+  end
+
+
+  
   def serial_libraries_list(records)
     res=[]; cnt=0
     records.each do |r|
@@ -62,6 +86,7 @@ module SerialTitlesHelper
       else
         lnk = ''
       end
+      lnk = '-'
       res << content_tag(:tr, content_tag(:td, r.sigla) +
                               content_tag(:td, r.nickname) +
                               content_tag(:td, r.label) +
@@ -95,13 +120,14 @@ module SerialTitlesHelper
     # links << link_to('Liste periodici', serial_lists_path) if params[:controller]!='lperiodici'
     links << link_to('Liste periodici', serial_lists_path)
 
-    if params[:controller]=='serial_titles' and ['new','create','edit','update','show','print'].include?(params[:action])
+    if ['serial_titles','serial_invoices'].include?(params[:controller]) and ['new','create','edit','update','show','print'].include?(params[:action])
       links << link_current_params(@serial_list.to_label, serial_titles_path,params)
     end
 
     if params[:controller]=='serial_titles' and params[:action] = 'show' and !params[:id].blank?
       links << link_current_params(content_tag(:b, @serial_title.title), serial_title_path,params)
     end
+
     if params[:action]=='index' and !@serial_list.nil?
       links << link_current_params(@serial_list.to_label, serial_titles_path,params)
     end
@@ -112,4 +138,71 @@ module SerialTitlesHelper
     %Q{&nbsp; / &nbsp;#{links.join('&nbsp; / &nbsp;')}}.html_safe
   end
 
+  def serial_titles_issues_report(r)
+    res=[]
+    cmds=[
+      :issue_descriptions,
+      :issue_status,
+      :issue_arrival_dates,
+      :issue_arrival_dates_expected,
+    ]
+
+    cmds.each do |cmd|
+      dat=r.send(cmd)
+      dat.gsub!(/{|}/,'')
+      ar=CSV.parse_line(dat)
+      item_ids=CSV.parse_line(r.send(:item_ids).gsub(/{|}/,'')) if cmd==:issue_descriptions
+      td=[]
+      i=0
+      # td << content_tag(:td, cmd[6..-1].capitalize)
+      while i < ar.size  do
+        if cmd==:issue_descriptions
+          td << content_tag(:td, link_to(ar[i], ClavisItem.clavis_url(item_ids[i]), :target=>'_new'))
+        else
+          td << content_tag(:td, ar[i])
+        end
+        i+=1
+      end
+      res << content_tag(:tr, td.join.html_safe, {title:cmd[6..-1].capitalize})
+    end
+    content_tag(:table, res.join.html_safe, {:style=>'width: 90%;', class: 'table table-bordered table-condensed table-striped'})
+  end
+
+  def invoice_editable(serial_title, invoice_id, library_id)
+    @serial_subscription=SerialSubscription.find(serial_title.id,library_id)
+    best_in_place(@serial_subscription, :prezzo, ok_button:'Salva', cancel_button:'Annulla',
+                  ok_button_class:'btn btn-success',
+                  class:'btn btn-default',
+                       skip_blur:false,
+                       :place_holder => "Prezzo in fattura qui",
+                  html_attrs:{size:10})
+  end
+
+  def serial_invoices_list(serial_list)
+    res = []
+    rr=nil
+    res << content_tag(:tr, content_tag(:td, 'Riferimento fattura') +
+                            content_tag(:td, 'Totale fattura') +
+                            content_tag(:td, 'Prezzo effettivo') +
+                            content_tag(:td, 'Prezzo stimato') +
+                            content_tag(:td, 'Differenza'))
+
+    serial_list.serial_invoices_report.each do |r|
+      rr = r and break if r.clavis_invoice_id.nil?
+      prezzo = r.total_amount.to_f == r.prezzo.to_f ? number_to_currency(r.prezzo) : "<b>#{number_to_currency(r.prezzo)}</b>"
+      diff = r.prezzo_stimato.to_f - r.prezzo.to_f
+      res << content_tag(:tr, content_tag(:td, link_to(r.to_label,serial_invoice_path(r))) +
+                              content_tag(:td, number_to_currency(r.total_amount)) +
+                              content_tag(:td, prezzo.html_safe) +
+                              content_tag(:td, number_to_currency(r.prezzo_stimato)) +
+                              content_tag(:td, number_to_currency(diff)))
+    end
+    prezzo = rr.total_amount.to_f == rr.prezzo.to_f ? number_to_currency(rr.prezzo) : "<b>#{number_to_currency(rr.prezzo)}</b>"
+    res << content_tag(:tr, content_tag(:td, 'Totali') +
+                            content_tag(:td, number_to_currency(rr.total_amount)) +
+                            content_tag(:td, prezzo.html_safe) +
+                            content_tag(:td, number_to_currency(rr.prezzo_stimato)) +
+                            content_tag(:td, number_to_currency(rr.prezzo_stimato.to_f - rr.prezzo.to_f)))
+    content_tag(:table, res.join.html_safe, {:style=>'width: 90%;', class: 'table table-bordered table-condensed table-striped'})
+  end
 end
