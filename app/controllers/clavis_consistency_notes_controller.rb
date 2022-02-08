af9b26e849243a689a8a876d7ac890e573debba9
sebastianomidolo <sebastiano.mdl@gmail.com>
2022-02-08 14:52:38 +0100

    Aggiunta range numerico di ricerca e colonna URL del periodico se presente

--- app/controllers/clavis_consistency_notes_controller.rb
+++ app/controllers/clavis_consistency_notes_controller.rb
@@ -3,26 +3,40 @@ class ClavisConsistencyNotesController < ApplicationController
 
   def index
     @clavis_consistency_note = ClavisConsistencyNote.new(params[:clavis_consistency_note])
+    # render text:@clavis_consistency_note.collocazione_per and return
     @clavis_consistency_note['title']=''
-    @pagetitle='Verifica note di consistenza per Civica centrale'
+    @pagetitle='Archivio periodici con collocazione "Per." in Civica centrale'
 
     cond=[]
     if !@clavis_consistency_note.collocazione_per.nil?
       cond << "cn.collocazione_per=#{@clavis_consistency_note.collocazione_per}"
     end
+
+    if !params[:per_from_to].blank?
+      from,to=params[:per_from_to].split('-')
+      cond << "cn.collocazione_per BETWEEN #{from.to_i} AND #{to.to_i}"
+    end
     if !@clavis_consistency_note.text_note.nil?
       cond << "cn.text_note ~* #{ClavisConsistencyNote.connection.quote(@clavis_consistency_note.text_note)}"
     end
     cond = cond.size==0 ? '' : "AND #{cond.join(' AND ')}"
+    url_sbn_join = 'left join'
     sql=%Q{select cn.collocazione_per,cn.consistency_note_id,cn.collocation,cn.text_note,trim(cm.title) as title,
-                cm.manifestation_id,
-                 array_agg(pic.cassa) as casse
+                cm.manifestation_id,cm.bid,
+                 array_agg(pic.cassa) as casse,
+   (case
+     when url_sbn.url is null
+       then ''
+       else url_sbn.url
+    end) as "URL"
+
         from clavis.consistency_note cn 
 left join clavis.periodici_in_casse pic on(cn.collocazione_per=pic.collocazione_per)
 join clavis.manifestation cm using(manifestation_id)
+#{url_sbn_join} clavis.url_sbn using(manifestation_id)
         where cn.collocazione_per >0 and library_id = 2 and cn.collocation ~* 'per'
           #{cond}
-          group by cn.consistency_note_id,cn.collocation,cn.text_note,trim(cm.title),cm.manifestation_id
+          group by cn.consistency_note_id,cn.collocation,cn.text_note,trim(cm.title),cm.manifestation_id,url_sbn.url
            order by cn.collocazione_per,replace(lower(cn.collocation),'bct.','')}
     @clavis_consistency_notes=ClavisConsistencyNote.paginate_by_sql(sql,:page=>params[:page], :per_page=>100)
   end
