0e3351f7424a7af8a385795655a5b45e5da4e19d
sebastianomidolo <sebastiano.mdl@gmail.com>
2022-02-08 14:43:34 +0100

    SQL per ricerce preconfezionate

---
+++ extras/sql/search/spostamento_libri_civica_-1.sql
@@ -0,0 +1,11 @@
+item.home_library_id=2
+AND item.item_media='F'
+AND item.section='BCT'
+AND NOT cl.secondo_elemento in ('LB','LC','LD','LF','LG','LM')
+AND NOT cl.primo_elemento between '67' and '69'
+AND NOT cl.primo_elemento between '400' and '405'
+AND NOT cl.primo_elemento between '407' and '408'
+AND NOT cl.primo_elemento between '410' and '413'
+AND cm.edition_date between 1600 and 1969
+AND NOT item.collocation ~* 'mano'
+AND cl.piano = '02 primo semint'
---
+++ extras/sql/search/spostamento_libri_civica_6_mp.sql
@@ -0,0 +1,12 @@
+item.home_library_id=2
+AND item.item_media='F'
+AND item.section='BCT'
+AND NOT cl.secondo_elemento in ('LB','LC','LD','LF','LG','LM')
+AND NOT cl.primo_elemento between '67' and '69'
+AND NOT cl.primo_elemento between '400' and '405'
+AND NOT cl.primo_elemento between '407' and '408'
+AND NOT cl.primo_elemento between '410' and '413'
+AND cm.edition_date between 1600 and 1969
+AND NOT item.collocation ~* 'mano'
+AND cl.piano = '6° piano'
+AND item.usage_count=0
